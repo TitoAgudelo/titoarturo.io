267b311c9ba4f15f1f77020f786990d7c3ac4520
tagudelo <titoarturoagudelo@gmail.com>
2014-07-09 12:49:07 -0500

    rest

---
+++ _rest/Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: [
+          // bower components
+            'bower_components/bootstrap-sass-official/assets/bootstrap.js',
+            'bower_components/scrollReveal.js/dist/scrollReveal.js',
+            'bower_components/modernizr/modernizr.js',
+
+          // internals
+          // 'js/src/app.js',
+          // 'js/src/filters.js',
+          // 'js/src/utils.js',
+          // 'js/src/controllers/*.js',
+          // 'js/src/services/*.js'
+        ],
+        dest: 'js/app.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
